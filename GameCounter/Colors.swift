61d368a0c5bcc11164c80ac61e267f8e5f4ad400
WhiteTestarossa <daniel.belokursky@gmail.com>
2022-09-23 10:48:05 +0300

    feat: add colors

---
+++ GameCounter/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  GameCounter
+//
+//  Created by Daniel Belokursky on 23.09.22.
+//
+
+import UIKit
+
+
+class Colors {
+    
+    static let shared = Colors()
+    
+    private init() {}
+    
+    let backgroundColor = UIColor.init(red: 35.0/255.0, green: 35.0/255.0, blue: 35.0/255.0, alpha: 1.0)
+    let buttonColor = UIColor.init(red: 132.0/255.0, green: 184.0/255.0, blue: 173.0/255.0, alpha: 1.0)
+    let backgroundForActive = UIColor.init(red: 59.0/255.0, green: 59.0/255.0, blue: 59.0/255.0, alpha: 1.0)
+    let deleteColor = UIColor.init(red: 236.0/255.0, green: 94.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+    let namesColor = UIColor.init(red: 235.0/255.0, green: 174.0/255.0, blue: 104.0/255.0, alpha: 1.0)
+    
+}
